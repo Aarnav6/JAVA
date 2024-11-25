122316b02ac2af74507fca0958d0a5be76c6a315
Aarnav6 <aarnavs1008@gmail.com>
2024-11-25 20:51:26 +0530

    working on sorting techniques

--- Java/Lec16.java
+++ Java/Lec16.java
@@ -1,16 +1,15 @@
 ////////////////////////////////////////////////Bubble_Sort TECHNIQUES///////////////////////////////////////////
 import java.util.Scanner;
-// import java.util.function.IntPredicate;
 class Array_Functions {
     protected static int size;
     protected static int InputArray[];
 
     public static void getArray () {
             Scanner scan = new Scanner(System.in);
-            System.out.println("enter the Length of array");
+            System.out.print("enter the Length of array:- ");
             size = scan.nextInt();
-            System.out.println("enter array elements");
             InputArray = new int [size];   
+            System.out.println("enter array elements");
             for (int i=0 ; i<size ; i++) {
             InputArray[i] = scan.nextInt();
         }
@@ -19,6 +18,7 @@ class Array_Functions {
             for (int i=0 ; i<InputArray.length ; i++) {
             System.out.print(InputArray[i] + "  ");
         }
+        System.out.print("\n");
     }
 }
 
@@ -34,6 +34,7 @@ class Bubble_Sort extends Array_Functions {
                 }
             }
         }
+        showArray();
     }
     public static void Bubble_SortArray_Descending() {
             for (int i=0 ; i<InputArray.length-1 ; i++) {
@@ -45,21 +46,64 @@ class Bubble_Sort extends Array_Functions {
                 }
             }
         }
+        showArray();
     }
 }
-@SuppressWarnings("unused")
-class Lec16 extends Bubble_Sort {
+class Selection_Sort extends Array_Functions {
 
-    public static void main (String args[]) {
-        
+    public static void Selection_SortArray_Ascending () {
+        for(int i=0 ; i<InputArray.length-1 ; i++) {
+            // System.out.println("Starting loop from i= " + i); // temporary understanding lines
+            int Smallest_Element_Index = i;
+            for(int j=i ; j<InputArray.length ; j++) {
+                // System.out.println("Starting loop from arrayIndex(j)= " + j);// temporary understanding lines
+                if(InputArray[Smallest_Element_Index] > InputArray[j]) {
+                    Smallest_Element_Index = j;
+                    // System.out.println(InputArray[j]  + " is smalllest in iteration of " + i); // temporary understanding lines
+                }
+            }
+            int Temp = InputArray[i];
+            InputArray[i] = InputArray[Smallest_Element_Index];
+            InputArray[Smallest_Element_Index] = Temp;
+            // System.out.println("Swapping " + InputArray[i] + " and " + Temp); // temporary understanding lines
+            // System.out.println("showing current array"); // temporary understanding lines
+            // showArray(); // temporary understanding lines
+            // System.out.println("\n \n \n ");
+        }
+        Array_Functions.showArray();
+    }
+    public static void Selection_SortArray_Descending () {
+        for(int i=0 ; i<InputArray.length-1 ; i++) {
+            int Biggest_Element_Index = i;
+            for(int j=i ; j<InputArray.length ; j++) {
+                if(InputArray[Biggest_Element_Index] < InputArray[i]) {
+                    Biggest_Element_Index = j;
+                }
+            }
+            int Temp = InputArray[i];
+            InputArray[i] = InputArray[Biggest_Element_Index];
+            InputArray[Biggest_Element_Index] = Temp;
+        }
+        Array_Functions.showArray();
+    }
+}
+
+class Lec16 {
+
+    Lec16() {
         Array_Functions.getArray();
-
-        System.out.print("\nPrinting Array in Ascending: ");
-        Bubble_Sort.Bubble_SortArray_Ascending();
-        Array_Functions.showArray();
         
-        System.out.print("\nPrinting Array in Descending: ");
+        System.out.println("Using bubbleSort for Ascending Order:- ");
         Bubble_Sort.Bubble_SortArray_Descending();
-        Array_Functions.showArray();
+        System.out.println("Using bubbleSort for Descending Order:- ");
+        Bubble_Sort.Bubble_SortArray_Ascending();
+
+        System.out.println("Using SelectionSort for Ascending Order:- ");
+        Selection_Sort.Selection_SortArray_Ascending();
+        System.out.println("Using SelectionSort for Descending Order:- ");
+        Selection_Sort.Selection_SortArray_Descending();
+    }
+    public static void main (String args[]) {
+    Lec16 ob = new Lec16();
     }
 }
