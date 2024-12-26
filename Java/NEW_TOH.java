b6784f9ad0c03d3066b29927c6440efff6ae395a
Aarnav6 <aarnavs1008@gmail.com>
2024-12-26 17:23:43 +0530

    updating work

--- Java/NEW_TOH.java
+++ Java/NEW_TOH.java
@@ -5,7 +5,7 @@ class Tower {
     private int Tow1[] = {10,20,30,40,50};
     private int Tow2[] = {0 ,0 ,0 ,0 ,0};
     private int Tow3[] = {0 ,0 ,0 ,0 ,0};
-    int disk;
+    int Element ,ElementNo ,TowerNo ,Disk;
     Scanner sc = new Scanner(System.in);
 
     public void Show() {
@@ -19,21 +19,24 @@ class Tower {
     }
     public void SelectForPick() {
         System.out.println("Tower no: ");
-        int TowerNo = sc.nextInt();
+        TowerNo = sc.nextInt();
+        if(TowerNo != 1 || TowerNo != 1 || TowerNo != 1) {
+            throw new RuntimeException("Not Valid Input");
+        }
         System.out.println("Element no: ");
-        int ElementNo = sc.nextInt();
+        ElementNo = sc.nextInt();
         switch(TowerNo-1) {
             case 0-> {
-                disk = 0;
-                System.out.println("picked the: " + Tow1[disk]+" Disk");
+                Disk = Tow1[ElementNo];
+                System.out.println("picked the: " + Tow1[Disk]+" Disk");
             }
             case 1 -> {
-                disk = 1;
-                System.out.println("picked the: " + Tow2[disk]+" Disk");
+                System.out.println("picked the: " + Tow2[Disk]+" Disk");
+                Disk = Tow2[ElementNo];
             }
             case 2 -> {
-                disk = 2;
-                System.out.println("picked the: " + Tow3[disk]+" Disk");
+                System.out.println("picked the: " + Tow3[Disk]+" Disk");
+                Disk = Tow3[ElementNo];
             }
         }
     }
