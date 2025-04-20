f74e5c239ed5f08d0455e5644ea942478e5db729
Aarnav <aarnavs1008@gmail.com>
2025-04-20 16:19:16 +0530

    updated

---
+++ Java/GetterAndSetter.java
@@ -0,0 +1,39 @@
+class GetterAndSetter {
+    private int x;
+
+    public int GetX() {
+        return x;
+    }
+    public void SetX(int in) {
+        this.x = in;
+    }
+}
+class run {
+/*
+        we cant directly acess the data members of the class through the object of class from outside the class
+        due to private acess modifiers so,
+        we acess them through the getter and setter function
+*/
+        public static void main(String[] args) {
+        GetterAndSetter ob = new GetterAndSetter();
+        // ob.x = 5; // throws an error due to the private acess specifier
+        System.out.println(ob.GetX());
+        
+    }
+}
+
+
+
+/*
+class a {
+    private int x; // gives an error 
+}
+
+class GetterAndSetter {
+    public static void main(String[] args) {
+        a ob = new a();
+        ob.x = 5; // ❌ This will now give error!
+        System.out.println(ob.x);
+    }
+}
+*/
---
+++ Java/Learning_Swing.java
@@ -0,0 +1,5 @@
+public class Learning_Swing {
+    public void createFrame(Learning_Swing ob) {
+        
+    }
+}
