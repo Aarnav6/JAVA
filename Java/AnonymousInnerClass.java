2197e347f43637fb416616aa6d17a4b4facb716d
Aarnav6 <aarnavs1008@gmail.com>
2025-05-18 16:35:20 +0530

    ad

--- Java/AnonymousInnerClass.java
+++ Java/AnonymousInnerClass.java
@@ -7,6 +7,7 @@ public class AnonymousInnerClass {
     public static void main(String[] args) {
         A object = new A()
         { // this is Anonymous Inner class which will create dlass files as $1 since it dosent has a name
+            @Override
             public void show () {
             System.out.println("new show function");
             }
