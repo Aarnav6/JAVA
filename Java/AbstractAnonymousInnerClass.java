adb76cf9a4a22b6e74a11d4782d7a871051a5dd9
Aarnav6 <aarnavs1008@gmail.com>
2025-05-18 16:48:58 +0530

    adding the Abstract Anonymous Inner Class code

---
+++ Java/AbstractAnonymousInnerClass.java
@@ -0,0 +1,19 @@
+abstract class A {
+    public void show () {
+        System.out.println("A class show function");
+    }
+    abstract public void Meth1();
+}
+public class AbstractAnonymousInnerClass {
+    public static void main(String[] args) {
+        A obj = new A() { // this is not an object of class A its an object of Anonymous Inner Class
+            @Override
+            public void Meth1() {
+                super.show(); // to call super class show funciton can be done is possible
+                System.out.println("meth1 overriden funciton of Anonymous Inner Class");
+            }
+        };
+        obj.show();
+        
+    }
+}
