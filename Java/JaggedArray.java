7b1a7b7e71e89b250b9e23104b5aa301702060b6
Aarnav6 <aarnavs1008@gmail.com>
2025-05-18 18:24:25 +0530

    adding jagged array code

---
+++ Java/JaggedArray.java
@@ -0,0 +1,28 @@
+public class JaggedArray {
+    public static void main(String[] args) {
+
+        int[][] arr = new int[3][];
+        arr[0] = new int [3];
+        arr[1] = new int [2];
+        arr[2] = new int [5];
+        
+        /* // can be used in this way also but indexed base loop is better for understanding
+        for (int[] i : arr) {
+            for (int j = 0; j < i.length; j++) {
+                arr1[j] = (int) (Math.random() * 10 );
+            }
+        }
+         */
+        for (int i=0 ; i < arr.length ; i++) {
+            for(int j=0 ; j<arr[i].length ; j++) {
+                arr[i][j] = (int) (Math.random() * 10 );
+            }
+        }
+        for(int[] i : arr) {
+            for(int j : i) {
+                System.out.print(j+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
