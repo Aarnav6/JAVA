d8121de009447cbff9ef86ca6e604f86f732914b
Aarnav6 <aarnavs1008@gmail.com>
2025-05-18 16:29:23 +0530

    added the "AnonymousInnerClass.java" code about inner anonymous class

---
+++ Java/AnonymousInnerClass.java
@@ -0,0 +1,16 @@
+class A {
+    public void show () {
+        System.out.println("A class show function");
+    }
+}
+public class AnonymousInnerClass {
+    public static void main(String[] args) {
+        A object = new A()
+        { // this is Anonymous Inner class which will create dlass files as $1 since it dosent has a name
+            public void show () {
+            System.out.println("new show function");
+            }
+        };
+        object.show();
+    }
+} 
