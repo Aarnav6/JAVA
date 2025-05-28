d1d19307036cb5874cf9e6f2353623db6433e511
Aarnav6 <aarnavs1008@gmail.com>
2025-05-28 16:10:49 +0530

    add leap year code

---
+++ Python/LeapYear.py
@@ -0,0 +1,8 @@
+year = int(input("enter the your year: "))
+if year % 4 == 0 :
+    if year % 100 != 0 or year % 400 == 0:
+        print("Leap Year")
+    else :
+        print("Not a Leap Year")
+else:
+    print("Not a Leap Year")
