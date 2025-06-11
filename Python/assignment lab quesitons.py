ea1f62dd7ea0ab423ac97d7091178c82c962ed70
Aarnav6 <aarnavs1008@gmail.com>
2025-06-12 05:06:46 +0530

    adding code about python list data structure

---
+++ Python/assignment lab quesitons.py
@@ -0,0 +1,15 @@
+'''
+note : all inputs musst be from user :-
+
+write a program in python to find simple intrest in python ?
+write a program to remove multiple elements from a list in python ?
+write a python program to concatenate tuples to nested tuples  ?
+write a python to check weather an age is elgible for voting or not ?
+write a program to print largest of three numbers  ?
+write a pyhton program to find first n prime numbers ?
+write a program to check weather number is armstrong or not ?
+write a program to perform binary search ?
+write a program to perform linear search ?
+write a program to find gcd of two numbers ?
+
+'''
