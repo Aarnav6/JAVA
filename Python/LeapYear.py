a7ebb75117a176df73ab4c7f355e190c40af9226
Aarnav <aarnavs1008@gmail.com>
2025-10-01 14:42:56 +0530

    Delete Python directory
    making this repository for java only

--- Python/LeapYear.py
+++
@@ -1,8 +0,0 @@
-year = int(input("enter the your year: "))
-if year % 4 == 0 :
-    if year % 100 != 0 or year % 400 == 0:
-        print("Leap Year")
-    else :
-        print("Not a Leap Year")
-else:
-    print("Not a Leap Year")
